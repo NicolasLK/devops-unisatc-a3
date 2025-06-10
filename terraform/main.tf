5959dd90c2b214c70778a3d67746ef8226fe0854
NicolasLK <nicolas.kaminski.2001@gmail.com>
2025-06-10 14:28:24 -0300

    fix: Adicionando informações no main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,19 @@
 resource "google_container_cluster" "devops-cluster" {
   name     = "gke-devops-cluster"
-  location = "us-central1"
+  location = "us-central1-a"
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
   remove_default_node_pool = true
   initial_node_count       = 1
+  default_max_pods_per_node = 30
+  deletion_protection = false
 }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
   name       = "node-devops-pool"
-  location   = "us-central1"
+  location   = "us-central1-a"
   cluster    = google_container_cluster.devops-cluster.name
   node_count = 1
 
@@ -20,7 +22,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     machine_type = "e2-medium"
 
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    service_account = google_service_account.default.email
+    # service_account = google_service_account.default.email
     oauth_scopes    = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
