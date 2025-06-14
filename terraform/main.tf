3d561e2d8eab3165542bdb53c061a4ee7c5875c4
NicolasLK <nicolas.kaminski.2001@gmail.com>
2025-06-14 19:20:46 -0300

    fix(main.tf): Evitando a exclusão da pool e atualização por mudanças

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,6 @@
 resource "google_container_cluster" "devops-cluster" {
   name     = "gke-devops-cluster"
   location = "us-central1-a"
-
-  # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
   remove_default_node_pool  = true
   initial_node_count        = 1
   default_max_pods_per_node = 30
@@ -20,9 +16,6 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   node_config {
     preemptible  = true
     machine_type = "e2-medium"
-
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    # service_account = google_service_account.default.email
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
@@ -31,5 +24,15 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   upgrade_settings {
     max_surge       = 1
     max_unavailable = 0
+  }
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes = [
+      node_config[0].kubelet_config,
+      node_config[0].resource_labels,
+      node_config[0].tags,
+      node_config[0].labels,
+    ]
   }
 }
