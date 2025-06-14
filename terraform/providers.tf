49babbcb382a72707f2294ecde1052b7eb0b9204
NicolasLK <nicolas.kaminski.2001@gmail.com>
2025-06-14 16:22:24 -0300

    fix(providers.tf): Corrige sintaxe do host no provider Kubernetes

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,7 +4,7 @@ provider "google" {
 }
 
 provider "kubernetes" {
-  host                   = "https://{google_container_cluster.devops-cluster.endpoint}"
+  host                   = "https://${google_container_cluster.devops-cluster.endpoint}"
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(google_container_cluster.devops-cluster.master_auth[0].cluster_ca_certificate)
 }
