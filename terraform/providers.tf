e47eb76cec7be40f26bfb836eff0c594f927fa0b
NicolasLK <nicolas.kaminski.2001@gmail.com>
2025-06-14 17:04:25 -0300

    fix(provider.tf): Remove bloco de timeout incorreto do provider Kubernetes

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,7 +9,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(google_container_cluster.devops-cluster.master_auth[0].cluster_ca_certificate)
 
   # Bloco para aumentar o tempo de espera
-  provider_timeout {
-    cluster_unhealthy = "10m"
-  }
+  # provider_timeout {
+  #   cluster_unhealthy = "10m"
+  # }
 }
